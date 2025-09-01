a5e61f2f72282582800178235b3dcb242d15aade
AAMENA123 <167003115+AAMENA123@users.noreply.github.com>
2025-09-01 14:45:52 +0530

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,9 +1,5 @@
 from bs4 import BeautifulSoup
 import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
 import pandas as pd
 import re
 
